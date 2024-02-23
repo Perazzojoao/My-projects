d102616cd33a8cc85bbdd11bda4616106e5fd3bc
Perazzojoao <perazzojoao@gmail.com>
2024-02-23 14:56:47 -0300

    Atualizações no Dockerfile e no código de conexão com o banco de dados

--- GO/Cadastro-api-rest/database/db.go
+++ GO/Cadastro-api-rest/database/db.go
@@ -18,12 +18,16 @@ var (
 )
 
 func DBConnection() {
-	err = godotenv.Load()
-	if err != nil {
-		log.Panic(err.Error())
+	host := "postgres"
+	if _, err := os.Stat(".env"); err == nil {
+		err = godotenv.Load()
+		if err != nil {
+			log.Panic(err.Error())
+		}
+		host = "localhost"
 	}
 
-	dns := fmt.Sprintf("host=localhost user=%s password=%s dbname=%s port=5432 sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	dns := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable", host, os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 	DB, err = gorm.Open(postgres.Open(dns))
 	if err != nil {
 		fmt.Println("Erro ao conectar com database")
